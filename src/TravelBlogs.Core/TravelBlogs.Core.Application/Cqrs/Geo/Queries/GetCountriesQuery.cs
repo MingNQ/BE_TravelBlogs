d9c83c148653b6d561688683c5d69205992eddf7
TranDucHai <leomessi020320003@gmail.com>
2025-10-09 18:33:14 +0700

    feat: get country, destination by countryId

---
+++ src/TravelBlogs.Core/TravelBlogs.Core.Application/Cqrs/Geo/Queries/GetCountriesQuery.cs
@@ -0,0 +1,20 @@
+using MediatR;
+using TravelBlogs.Core.Application.Common.Persistences;
+using TravelBlogs.Core.Application.Cqrs.Geo.Specs;
+using TravelBlogs.Core.Application.Dto.Integrates.Geo;
+using TravelBlogs.Core.Domain.Entities.Geo;
+
+namespace TravelBlogs.Core.Application.Cqrs.Geo.Queries;
+
+public class GetCountriesQuery : IRequest<List<CountryDto>>;
+
+public class GetCountriesQueryHandler(IReadRepository<Country> repository)
+    : IRequestHandler<GetCountriesQuery, List<CountryDto>>
+{
+    public async Task<List<CountryDto>> Handle(GetCountriesQuery request, CancellationToken cancellationToken)
+    {
+        var spec = new CountryForExternalSpec();
+        var list = await repository.ListAsync(spec, cancellationToken);
+        return list;
+    }
+}
