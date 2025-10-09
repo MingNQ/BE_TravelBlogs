7d5f583573180e3049f3dac494d0a1902953a8b9
Ming <minhlaminh01@gmail.com>
2025-10-10 00:25:31 +0700

    fix: bind full path for file storage

--- src/TravelBlogs.Core/TravelBlogs.Core.Application/Cqrs/FileStorages/Queries/GetFileStorageByIdQuery.cs
+++ src/TravelBlogs.Core/TravelBlogs.Core.Application/Cqrs/FileStorages/Queries/GetFileStorageByIdQuery.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using TravelBlogs.Core.Application.Common.Exceptions;
 using TravelBlogs.Core.Application.Common.Persistences;
+using TravelBlogs.Core.Application.Common.Services;
 using TravelBlogs.Core.Application.Cqrs.FileStorages.Specs;
 using TravelBlogs.Core.Application.Dto.Persistence.Catalog.FileStorages;
 using TravelBlogs.Core.Domain.Entities.Common;
@@ -13,7 +14,9 @@ public class GetFileStorageByIdQuery : IRequest<FileStorageDto>
     public long Id { get; set; }
 }
 
-public class GetFileStorageByIdQueryHandler(IReadRepository<FileStorage> fileStorageRepository)
+public class GetFileStorageByIdQueryHandler(
+    IReadRepository<FileStorage> fileStorageRepository,
+    IFilePathService filePathService)
     : IRequestHandler<GetFileStorageByIdQuery, FileStorageDto>
 {
     public async Task<FileStorageDto> Handle(GetFileStorageByIdQuery request, CancellationToken cancellationToken)
@@ -21,6 +24,7 @@ public class GetFileStorageByIdQueryHandler(IReadRepository<FileStorage> fileSto
         var spec = new FileStorageByIdSpec(request.Id);
         var fileStorage = await fileStorageRepository.FirstOrDefaultAsync(spec, cancellationToken)
             ?? throw new NotFoundException(MessageCommon.SetEntityNotFound(nameof(FileStorage), request.Id));
+        filePathService.BindFullPaths(fileStorage);
 
         return fileStorage;
     }
