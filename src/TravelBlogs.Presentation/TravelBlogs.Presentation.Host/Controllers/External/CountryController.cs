e0355dccc4e49e3d45627f0d3b6a928c28d4b237
TranDucHai <leomessi020320003@gmail.com>
2025-10-09 21:59:43 +0700

    fix: country/{id}/destinations

--- src/TravelBlogs.Presentation/TravelBlogs.Presentation.Host/Controllers/External/CountryController.cs
+++ src/TravelBlogs.Presentation/TravelBlogs.Presentation.Host/Controllers/External/CountryController.cs
@@ -5,7 +5,7 @@ using TravelBlogs.Presentation.Host.Controllers.Base;
 
 namespace TravelBlogs.Presentation.Host.Controllers.External;
 
-[ControllerName("countries")]
+[ControllerName("country")]
 [Tags("External|Geography")]
 public class CountryController : BaseNoAuthController
 {
@@ -13,6 +13,13 @@ public class CountryController : BaseNoAuthController
     public async Task<IActionResult> GetAsync()
     {
         var result = await Mediator.Send(new GetCountriesQuery());
+        return Ok(result);
+    }
+
+    [HttpGet("{countryId:long}/destinations")]
+    public async Task<IActionResult> GetDestinationsByCountryAsync(long countryId)
+    {
+        var result = await Mediator.Send(new GetDestinationsByCountryQuery(countryId));
         return Ok(result);
     }
 }
